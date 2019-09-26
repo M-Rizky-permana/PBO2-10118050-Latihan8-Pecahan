0e52e6dc3a782a2018f3a9be7ff368352dac962c
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 20:03:19 +0700

    Add files via upload

---
+++ PBO210118050Latihan8Pecahan/src/pbo210118050latihan8pecahan/PBO210118050Latihan8Pecahan.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210118050latihan8pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan bilangan pecahan
+ */
+public class PBO210118050Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_576_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+        
+        
+        
+        
+        
+    }
+    
+}
